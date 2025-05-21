e2d40b85fce4bcc2b3d0cc96517d81cf9df55872
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-05-21 14:34:14 -0300

    chore: adiciona as daos do bot

---
+++ daos/uc_dao.py
@@ -0,0 +1,29 @@
+import sqlite3
+from models.uc import UC
+
+class UCDao:
+    def buscar_uc(numero: str) -> UC | None:
+        conn = sqlite3.connect("database/uc.db")
+        cursor = conn.cursor()
+
+        cursor.execute("""
+        SELECT 
+            LATITUDE,
+            LONGITUDE
+        FROM 
+            UC
+        WHERE 
+            UC = ?
+        """, (numero,))
+
+        resultado = cursor.fetchone()
+        conn.close()
+
+        if resultado:
+            latitude, longitude = resultado
+            return True, UC(numero=numero, latitude=latitude, longitude=longitude)
+        else:
+            return False, "⚠️ UC incorreta ou não possui coordenadas cadastradas no banco."
+       
+        
+        
