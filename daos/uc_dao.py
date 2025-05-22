ecdaed3bd3d50687060aa140c158ec24b0347cae
ROMULO BARRETO DA SILVA <romubah@gmail.com>
2025-05-22 14:14:35 -0300

    refactor: ajusta o caminho do banco de dados das UCs

--- daos/uc_dao.py
+++ daos/uc_dao.py
@@ -1,27 +1,28 @@
 import sqlite3
+import os
 from models.uc import UC
 
 class UCDao:
     @staticmethod
     def buscar_uc(numero: str) -> tuple[bool, str]:
-        conn = sqlite3.connect("database/uc.db")
-        cursor = conn.cursor()
+        try:
+            BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+            db_path = os.path.join(BASE_DIR, "..", "database", "uc.db")
 
-        cursor.execute("""
-        SELECT 
-            LATITUDE,
-            LONGITUDE
-        FROM 
-            UC
-        WHERE 
-            UC = ?
-        """, (numero,))
+            conn = sqlite3.connect(db_path)
+            cursor = conn.cursor()
 
-        resultado = cursor.fetchone()
-        conn.close()
+            cursor.execute("""
+                SELECT LATITUDE, LONGITUDE FROM UC WHERE UC = ?
+            """, (numero,))
 
-        if resultado:
-            latitude, longitude = resultado
-            return True, UC(numero=numero, latitude=latitude, longitude=longitude)
-        else:
-            return False, "⚠️ UC incorreta ou não possui coordenadas cadastradas no banco."
+            resultado = cursor.fetchone()
+            conn.close()
+
+            if resultado:
+                latitude, longitude = resultado
+                return True, UC(numero=numero, latitude=latitude, longitude=longitude)
+            else:
+                return False, "⚠️ UC incorreta ou não possui coordenadas cadastradas no banco."
+        except Exception as e:
+            return False, f"❌ Erro ao acessar o banco: {str(e)}"
